6f05fffbf9f1a050f153d01c78b1ccd30eef0b7c
Valentinka <99435033+heapoflove@users.noreply.github.com>
2022-08-13 21:50:37 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-let userName = prompt("Введите свое имя:")
-
-let myTitle = document.getElementById("title")
+const userName = prompt("Введите Имя: ")
+let count = 0
 
 if (userName) {
-    myTitle.innerHTML = "Привет, " + userName
+    document.getElementById('title').innerHTML = "Ваш список дел, " + userName + ":"
 } else {
-    myTitle.innerHTML = "Привет, анонимный пользователь"
+    document.getElementById('title').innerHTML = "Список дел анонимного пользователя"
+}
+function addTask() {
+    count++ 
+    if (count > 5) {
+        document.getElementById('error').innerHTML = "Можно ввести 5 дел"
+        return
+    }
+    const input = document.getElementById('input')
+    const task = input.value
+
+    const myList = document.getElementById('list')
+    myList.innerHTML = myList.innerHTML + '<li>' + task + '<li>'
+    input.value = ""
+
 }
